3dae4d659494dbf3a99f2b492ec02948b48186f3
Jamal Lasri <jamal.lasri@redslim.net>
2021-08-20 15:23:24 +0200

    gffg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { ProductNavBarComponent } from './components/products/product-nav-bar/product-nav-bar.component';
+import { ProductsComponent } from './components/products/products.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductNavBarComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/products/product-nav-bar/product-nav-bar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-nav-bar',
+  templateUrl: './product-nav-bar.component.html',
+  styleUrls: ['./product-nav-bar.component.css']
+})
+export class ProductNavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getAllProducts(){
+
+  }
+
+  getSelectedProducts(){
+
+  }
+
+  getAvailableProducts(){
+
+  }
+
+  addProduct(){
+
+  }
+
+  onSearch(value: string){
+
+  }
+}
